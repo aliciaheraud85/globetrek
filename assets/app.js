85a92b91e0dd39f1732fb5b134e03dd8176a0f24
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-10-14 10:37:30 +0200

    modif link css

--- assets/app.js
+++ assets/app.js
@@ -5,12 +5,5 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/index.css';
-import './styles/header.css';
-import './styles/footer.css';
-import './styles/login.css';
-import './styles/register.css';
-import './styles/show.css';
-import './styles/categories.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
